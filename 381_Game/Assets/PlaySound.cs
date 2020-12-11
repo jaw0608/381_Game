6f03289a0c8dcb941d8ab2593e51823014e43a30
Joe Wilson <joseph.wilson@stonybrook.edu>
2020-12-11 12:48:21 -0500

    Fixed level 2 failing and added final scene

---
+++ 381_Game/Assets/PlaySound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class PlaySound : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData data)
+    {
+        sound.Play(0);
+    }
+
+    public void OnPointerExit(PointerEventData data)
+    {
+        sound.Stop();
+    }
+}
